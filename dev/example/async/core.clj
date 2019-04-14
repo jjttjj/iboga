250531fb5f2e41028cd1b17785c3d381c37fdb9f
Justin Tirrell <jjttjj@gmail.com>
2019-04-14 17:58:15 +0000

    demo request-historical-data and request-matching symbols

--- dev/example/async/core.clj
+++ dev/example/async/core.clj
@@ -6,76 +6,85 @@
            java.sql.Timestamp
            com.ib.client.Contract))
 
-
-
-;;https://dev.clojure.org/jira/browse/CLJ-1451?page=com.atlassian.jira.plugin.system.issuetabpanels:all-tabpanel
-(defn take-until
-  ([pred]
-   (fn [rf]
-     (fn
-       ([] (rf))
-       ([result] (rf result))
-       ([result input]
-        (if (pred input)
-          (ensure-reduced (rf result input))
-          (rf result input))))))
-  ([pred coll]
-   (lazy-seq
-    (when-let [s (seq coll)]
-      (if (pred (first s))
-        (cons (first s) nil)
-        (cons (first s) (take-until pred (rest s))))))))
-
-(defn req-id [msg] (or (:reqId msg) (:tickerId msg)))
-
-(def ib-datetime-formatter (DateTimeFormatter/ofPattern "yyyyMMdd[  HH:mm:ss]"))
-
-(defn parse-time [s]
-  (if (= (count s) 8)
-    (-> s (LocalDate/parse ib-datetime-formatter) (LocalDateTime/of LocalTime/MIDNIGHT))
-    (-> s (LocalDateTime/parse ib-datetime-formatter))))
-
-(defn mkbar [bar]
-  {:time   (-> (.time bar) parse-time Timestamp/valueOf)
-   :open   (.open bar)
-   :high   (.high bar)
-   :low    (.low bar)
-   :close  (.close bar)
-   :volume (.volume bar)
-   :count  (.count bar)
-   :wap    (.wap bar)})
-
-
 (def log (atom []))
-(def responses (atom []))
-
-(def connection
-  (ib/async-conn "192.168.1.155" 7496 (rand-int (Integer/MAX_VALUE))
-                 #(do ;;(println "default msg handler:" %)
-                      (swap! log conj %))))
 
 (def ES (doto (Contract.)
           (.secType "CONTFUT")
           (.exchange "GLOBEX")
           (.symbol "ES")))
 
+#_(def connection
+  (ib/async-conn "192.168.0.1" 7496 (rand-int (Integer/MAX_VALUE))
+                 #(do ;;(println "default msg handler:" %)
+                    (swap! log conj %))))
 
-(ib/req! connection :reqHistoricalData
-         [(rand-int (Integer/MAX_VALUE))
-          ES
-          (.format (LocalDateTime/now) (DateTimeFormatter/ofPattern "yyyyMMdd[  HH:mm:ss]"))
-          "100 D" "5 mins"
-          "TRADES"
-          0
-          1
-          false
-          nil]
-         (fn [argmap]
-           (comp ;;(filter (fn [msg] (= (:type msg) :historicalData)))
-            (filter #(= (req-id argmap) (req-id %))) ;;weird tickerId vs reqId names
-            (map #(if (:bar %)
-                    (update % :bar mkbar)
-                    %))
-            (take-until #(= (:type %) :historicalDataEnd))
-            (map #(do ;;(println "historical data received: " %)
-                      (swap! responses conj %))))))
+
+
+;;request for historical data
+#_
+(let [fin    (promise)
+      data   (atom [])
+      req-id (rand-int (Integer/MAX_VALUE))]
+  (ib/request-historical-data connection
+                              req-id
+                              ES
+                              (LocalDateTime/now) ;; no end date for live updates
+                              "2 D" "1 hour"
+                              "TRADES"
+                              false ;;use rth
+                              1 ;;"2" here can make it an int.
+                              false ;;update
+                              
+                              {:data  #(swap! data conj %)
+                               :error #(do (println %)
+                                           (if (-> % :errorCode (= 162))
+                                             (do (println "Error Code 162: redoing request with '3 D' duration")
+                                                 
+                                                 )
+                                             (deliver fin nil)))
+                               :update #(do (println "Update received:" %))
+                               :end    #(do (println "Initial historical data received" %)
+                                            (deliver fin @data))})
+  @fin)
+
+
+;;request-historical-data with live updates
+#_
+(let [fin     (promise)
+      data    (atom [])
+      updates (atom [])
+      req-id  (rand-int (Integer/MAX_VALUE))
+      _
+      (ib/request-historical-data connection
+                                  req-id
+                                  ES
+                                  nil ;; no end date for live updates
+                                  "2 D" "1 hour"
+                                  "TRADES"
+                                  false ;;use rth
+                                  1 ;;"2" here can make it an int.
+                                  true ;;update
+                                  {:data       #(swap! data conj %)
+                                   :error      (fn [msg] (do (println "Error:" msg)))
+                                   :end        (fn [msg] (do (println "Initial historical data received")
+                                                             (deliver fin @data)))
+                                   :update     #(do (println "Update received:" %)
+                                                    (swap! updates conj %))
+                                   :update-end (fn [msg] (println "Update ended:" msg))})
+      result  {:initial-data @fin
+               :updates      updates
+               :req-id       req-id
+               :cancel       (fn [] (ib/cancel-historical-data connection req-id))} ]
+
+
+  (Thread/sleep 2000)
+  ((:cancel result)))
+
+
+;;symbol search
+#_
+(ib/request-matching-symbols
+ connection (rand-int 1000)
+ "SPY"
+ {:data (fn [data]
+          (run! #(println %) data))})
