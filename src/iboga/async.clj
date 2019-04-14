a84d78aff798fb1753964bfed05e967c7fc3b0af
Justin Tirrell <jjttjj@gmail.com>
2019-04-14 17:57:43 +0000

    add request-historical-data and request-matching-symbols

--- src/iboga/async.clj
+++ src/iboga/async.clj
@@ -1,6 +1,8 @@
 (ns iboga.async
   (:require [clojure.core.async :as a])
-  (:import [com.ib.client EClient EClientSocket EJavaSignal EReader EWrapper]))
+  (:import [com.ib.client EClient EClientSocket EJavaSignal EReader EWrapper]
+           [java.time LocalDate LocalDateTime LocalTime]
+           java.time.format.DateTimeFormatter))
 
 
 ;;EWrapper;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -58,7 +60,9 @@
 
 ;;Async Connection;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def default-buf-size 12048)
+;;temporary fix to allow large historical data requests
+;;This would ideally be a much lower default and configurable per request.
+(def default-buf-size 12048) 
 
 (defn handle-msgs
   ([ch] (handle-msgs ch nil))
@@ -68,7 +72,8 @@
        (do
          (when handler (handler msg))
          (recur))
-       (println "channel closed")))))
+       ;;(println "channel closed")
+       ))))
 
 (defn async-conn
   ([] (async-conn "127.0.0.1" 7496 (rand-int (Integer/MAX_VALUE))))
@@ -108,3 +113,107 @@
                                                  (to-array arglist))
 
     (handle-msgs ch)))
+
+;;for requests that do not expect or do anything with a response
+(defn do! [conn args req-type]
+  (req! conn req-type args (fn [_] (take 0)))
+  nil)
+
+;;General Util;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;From https://github.com/weavejester/medley/blob/1.1.0/src/medley/core.cljc#L263
+
+(defn take-upto
+  "Returns a lazy sequence of successive items from coll up to and including
+  the first item for which `(pred item)` returns true."
+  ([pred]
+   (fn [rf]
+     (fn
+       ([] (rf))
+       ([result] (rf result))
+       ([result x]
+        (let [result (rf result x)]
+          (if (pred x)
+            (ensure-reduced result)
+            result))))))
+  ([pred coll]
+   (lazy-seq
+    (when-let [s (seq coll)]
+      (let [x (first s)]
+        (cons x (if-not (pred x) (take-upto pred (rest s)))))))))
+
+;;Gateway;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def ib-datetime-formatter (DateTimeFormatter/ofPattern "yyyyMMdd[  HH:mm:ss]"))
+
+(defn parse-time [s]
+  (if (= (count s) 8)
+    (-> s (LocalDate/parse ib-datetime-formatter) (LocalDateTime/of LocalTime/MIDNIGHT))
+    (-> s (LocalDateTime/parse ib-datetime-formatter))))
+
+(defn msg-id [msg] (or (:reqId msg) (:tickerId msg) (:id msg)))
+
+(defn rand-id [] (rand-int (Integer/MAX_VALUE)))
+
+(defn bool->bit [bool] (case bool true 1 false 0))
+
+(defn ids-match-xf [argmap]
+  (filter #(= (msg-id argmap) (msg-id %))))
+
+(defn request-historical-data [conn req-id contract end-date duration
+                               bar-size what-to-show use-rth? format-date
+                               update?
+                               {:keys          [data error end update-end]
+                                update-handler :update}]
+  (let [mkbar       (fn [bar]
+                      {:open  (.open bar)  :high   (.high bar)   :low   (.low bar)
+                       :close (.close bar) :volume (.volume bar) :count (.count bar)
+                       :wap   (.wap bar)
+                       :time  (-> (.time bar) parse-time)})
+        ->ib        {:endDateTime #(.format % ib-datetime-formatter)
+                     :useRTH      bool->bit}
+        update-end? (fn [{:keys [type errorCode errorMsg] :as msg}]
+                      (and (= type :error) (= errorCode 162)
+                           (when (re-find #"API historical data query cancelled" errorMsg)
+                             true)))
+        cb          #(condp = (:type %)
+                       :historicalData       (when data (data (:bar %)))
+                       :historicalDataUpdate (when update-handler (update-handler (:bar %)))
+                       :error                (if (and update? (update-end? %))
+                                               (when update-end (update-end %))
+                                               (when error (error %)))
+                       :historicalDataEnd    (when end (end %)))
+        finished?   (if update?
+                      update-end?
+                      (comp #{:historicalDataEnd :error} :type))]
+    (req! conn :reqHistoricalData
+          [req-id
+           contract
+           (when end-date ((->ib :endDateTime) end-date))
+           duration
+           bar-size
+           what-to-show
+           ((->ib :useRTH) use-rth?) ;;use rth
+           format-date ;;"2" here can make it an int.
+           update? 
+           nil ;;unused extra arg
+           ]
+          (fn [argmap]
+            (comp
+             (ids-match-xf argmap)
+             (map #(if (:bar %) (update % :bar mkbar) %))
+             (take-upto finished?)
+             (map #(do (cb %) %)))))))
+
+(defn cancel-historical-data [conn req-id]
+  (do! conn [req-id] :cancelHistoricalData))
+
+
+(defn request-matching-symbols [conn req-id pattern {:keys [data]}]
+  (let [cb #(condp = (:type %)
+              :symbolSamples (when data (data (:contractDescriptions %))))]
+    (req! conn :reqMatchingSymbols [req-id pattern]
+          (fn [argmap]
+            (comp
+             (filter #(= (msg-id argmap) (msg-id %)))
+             (map #(do (cb %) %)))))))
