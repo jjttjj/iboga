efd9dd3f5531593e3c922d3b8aad6058b3b26daa
Justin Tirrell <jjttjj@gmail.com>
2019-08-07 18:13:46 -0400

    fix improper hashing of parameter names during init

--- dev/iboga/meta/init.clj
+++ dev/iboga/meta/init.clj
@@ -1,14 +1,17 @@
 (ns iboga.meta.init
   (:require [iboga.meta :refer [method-data]]))
 
+(defn hash-param [p]
+  (hash (str (.getDeclaringExecutable p) (hash p))))
+
 ;;note: requires "-parameters" javac argument to get meaningful param names
 (defn parameter-names [clazz]
   (->> (.getDeclaredMethods clazz)
        (mapcat
         #(for [p (.getParameters %)]
-           [(hash p) (.getName p)]))
+           [(hash-param p) (.getName p)]))
        (into {})))
 
-(spit "resources/parameter-names.edn"
+#_(spit "resources/parameter-names.edn"
       (merge (parameter-names com.ib.client.EClient)
              (parameter-names com.ib.client.EWrapper)))
