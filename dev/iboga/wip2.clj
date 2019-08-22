91f7db3d2bc95e91535d06712428ba14b92e700a
Justin Tirrell <jjttjj@gmail.com>
2019-08-22 16:01:26 -0400

    experimental acc-prom record type

--- dev/iboga/wip2.clj
+++ dev/iboga/wip2.clj
@@ -1,22 +1,22 @@
 (ns iboga.wip2
   (:require [iboga.core :as ib :refer :all]
-            [medley.core :as m]))
+            [medley.core :as m]
+            [clojure.tools.logging :as log]))
 
-(defn mkadder
+(defn mkputter
   "Returns a function which, given a value, adds that value the the atom
   a with the reducing function rf. If the result is reduced, deliver
   the dereferenced result to promise p."
   [a p rf]
   (fn [x]
     (let [result (rf a x)]
-      (if (reduced? result)
-        (deliver p @@result)
-        result)
+      (when (reduced? result)
+        (deliver p @@result))
       result)))
 
 (defn acc
   "Accumulates state in an atom subject to a transducer. Returns a map
-  with the keys :add!, :a and :p. Use the :add! function to add
+  with the keys :put!, :a and :p. Use the :put! function to add
   state. :p is a promise which will be delivered the state in a when
   rf is realized"
   ([xf rf] (acc xf rf (rf)))
@@ -25,7 +25,43 @@
          swapper (fn [acc x] (swap! acc rf x) acc)
          rf      (xf swapper)
          p       (promise)]
-     {:add! (mkadder a p rf) :a a :p p})))
+     {:_put! (mkputter a p rf) :a a :p p})))
+
+(defprotocol IPut
+  (put! [this x]))
+
+(defprotocol ICallback
+  (on-realized [this f]))
+
+(defrecord AccProm [a p _put! cbs]
+  IPut
+  (put! [this x]
+    (let [result (_put! x)]
+      (if (reduced? result)
+        (let [x @@result]
+          (doseq [cb @cbs]
+            (cb x)))
+        result)
+      nil))
+  ICallback
+  (on-realized [this f]
+    (assert (not (realized? p)))
+    (swap! cbs conj f))
+  clojure.lang.IDeref
+  (deref [this]
+    (deref p))
+  clojure.lang.IBlockingDeref
+  (deref [this timeout-ms timeout-val]
+    (deref p timeout-ms timeout-val)))
+
+(defn acc-prom
+  ([xf rf] (acc-prom xf rf (rf)))
+  ([xf rf init]
+   (map->AccProm (assoc (acc xf rf init) :cbs (atom [])))))
+
+;;todo: fill out protocols, see clojure.core/promise
+;;todo: AccAtom for non-sync responses.
+
 
 ;;payload/taker could have special meansings for keyword
 ;;(ie keep key for payload and take until keyword msg-key for taker)
@@ -55,10 +91,9 @@
     :payload (fn [_ctx] (keep (comp :contract-details second)))}
 
    :head-timestamp
-   {:taker (fn [_ctx] (take 1))
-    :payload (fn [_ctx] (keep (comp :head-timestamp second)))
+   {:taker   (fn [_ctx] (take 1))
+    :payload (fn [_ctx] (keep (fn [[_ msg]] (:head-timestamp msg))))
     :cleanup (fn [{:keys [conn req-key args]}]
-
                ;;head-timestamp needs to be cancelled or else the id cannot be
                ;;used again. This is unlike details/history which free the id
                ;;after results. Probably a bug in IB's api server.
@@ -108,7 +143,7 @@
   exist. A 'response-xf' describes the transformation of a stream of all
   received messages to a response"
   [{:keys [response-xf] :as ctx} xf-fn]
-  (let [xf        (xf-fn ctx)
+  (let [xf          (xf-fn ctx)
         response-xf (if response-xf (comp response-xf xf) xf)]
     (assoc ctx :response-xf response-xf)))
 
@@ -122,23 +157,21 @@
       (comp-response-xf ctx f)
       ctx)))
 
-(defn on-realized-response [{:keys [response-atom response-prom] :as ctx} f]
-  (add-watch response-atom (gensym)
-             (fn [k r o n]
-               (when (realized? response-prom)
-                 (f ctx)
-                 (remove-watch r k))))
-  ctx)
-
 (defn acc-response [{:keys [conn response-xf] :as ctx}]
+  (let [accp (acc-prom response-xf conj)
+        h    (fn [msg] (put! accp msg))
 
-  (let [{:keys [add! p a]} (acc response-xf conj)
-        h                  (fn [msg] (add! msg))
-
-        ctx (assoc ctx :response-prom p :response-atom a)]
+        ctx (assoc ctx :response-prom accp)]
     (add-handler conn h)
-    (on-realized-response ctx (fn [_] (println "removing handler") (remove-handler conn h)))
-    (on-realized-response ctx (fn [ctx] (println "cleanup") (cleanup ctx)))
+    (on-realized accp (fn [_]
+                        (log/trace "removing handler")
+                        (remove-handler conn h)))
+
+    ;;on-realization, cleanup will be passed the ctx as it was at this point
+    ;;will this always be sufficient?
+    (on-realized accp (fn [_]
+                        (log/trace "cleanup")
+                        (cleanup ctx)))
     ctx))
 
 (defn add-id* [args id]
@@ -153,30 +186,30 @@
 
 (comment
 
- (def SPY {:local-symbol "SPY" :sec-type "STK" :exchange "SMART" :currency "USD"})
+  (def SPY {:local-symbol "SPY" :sec-type "STK" :exchange "SMART" :currency "USD"})
 
- (def log (atom []))
+  (def log (atom []))
 
- (def conn (ib/client (fn [msg] (swap! log conj msg))))
+  (def conn (ib/client (fn [msg] (swap! log conj msg))))
 
- (ib/connect conn "localhost" 7497)
+  (ib/connect conn "localhost" 7497)
 
- (defn smart-req [conn req]
-   (-> (req-ctx conn req)
-       add-id          ;;force-adds an id to a request
-       prep-req        ;;standard core prep
-       sync-response   ;;synchronous response
-       payload-response ;;only return payloads
-       acc-response     ;;accumulator for responses
-       send-req         ;;send request
-       :response-prom
-       deref))  
+  (defn smart-req [conn req]
+    (-> (req-ctx conn req)
+        add-id          ;;force-adds an id to a request
+        prep-req        ;;standard core prep
+        sync-response   ;;synchronous response
+        payload-response ;;only return payloads
+        acc-response     ;;accumulator for responses
+        send-req         ;;send request
+        :response-prom
+        deref))  
 
 
- (smart-req conn [:historical-data [SPY]])
+  (smart-req conn [:historical-data [SPY]])
 
- (smart-req conn [:contract-details [SPY]])
+  (smart-req conn [:contract-details [SPY]])
 
- ;;note: returns vector of results even though it will only ever be one result 
- (smart-req conn [:head-timestamp [SPY]])
- )
+  ;;note: returns vector of results even though it will only ever be one result 
+  (smart-req conn [:head-timestamp [SPY]])
+  )
