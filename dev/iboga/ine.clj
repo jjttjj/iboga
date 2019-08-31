7c483973e79b3449d33798119dc02a6cfa9022ec
Justin Tirrell <jjttjj@gmail.com>
2019-08-31 14:22:01 -0400

    add iboga.ine

---
+++ dev/iboga/ine.clj
@@ -0,0 +1,13 @@
+(ns iboga.ine
+  (:require [iboga.core :as iboga]
+            [iboga.ine.defaults :as dflt]))
+
+(dflt/def-req-specs)
+
+(defn req [conn request]
+  (-> (iboga/req-ctx conn request)
+      iboga/assert-connected
+      iboga/ensure-argmap
+      iboga/maybe-validate
+      dflt/add-default-args
+      iboga/send-req))
